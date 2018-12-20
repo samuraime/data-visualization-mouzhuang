6a0d7be9315cb82006e0044d1f89e405cdefbd7b
BinkiZhao <samurai7@foxmail.com>
2018-12-20 17:21:51 +0800

    Updates

---
+++ precache-manifest.2aad1e9652630b000c18b6e52250a464.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "786402c1bdcce5f63b751ac64fa7580a",
+    "url": "/samuraime/data-visualization-mouzhuang/static/media/south.786402c1.png"
+  },
+  {
+    "revision": "2ef20f4f25f2ba5a5f6b",
+    "url": "/samuraime/data-visualization-mouzhuang/static/js/runtime~main.2ef20f4f.js"
+  },
+  {
+    "revision": "119cc1ffa04c0cbd208b",
+    "url": "/samuraime/data-visualization-mouzhuang/static/js/main.119cc1ff.chunk.js"
+  },
+  {
+    "revision": "38b2f16dc4934047da3f",
+    "url": "/samuraime/data-visualization-mouzhuang/static/js/1.38b2f16d.chunk.js"
+  },
+  {
+    "revision": "119cc1ffa04c0cbd208b",
+    "url": "/samuraime/data-visualization-mouzhuang/static/css/main.7a4d1045.chunk.css"
+  },
+  {
+    "revision": "ffb3971ada62ecc959f572ecb3013295",
+    "url": "/samuraime/data-visualization-mouzhuang/index.html"
+  }
+];
